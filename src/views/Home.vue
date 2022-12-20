21636c74c10b4e34e2ce1f434a0e7831685d96b2
Samridhi <83508209+SAMRIDHISAINII@users.noreply.github.com>
2022-12-20 09:52:45 +0530

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <section id="hero">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center"
+             data-aos="fade-up">
+          <div>
+            <h1>Landscape change smart tracker</h1>
+            <h2>Machine learning-driven service for image comparison</h2>
+            <a href="/upload" class="btn-get-started scrollto">Upload your image</a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img">
+          <img :src="this.logo" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data: function() {
+    return {
+      logo: require('../assets/hero.png')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
